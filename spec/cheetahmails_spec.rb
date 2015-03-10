362d39f7280308e2ee4c102e812ba3be14b57d6f
Adam Butler <adam@lab.io>
2015-03-10 23:48:47 +0000

    Refactor Part 1

--- spec/cheetahmails_spec.rb
+++ spec/cheetahmails_spec.rb
@@ -1,105 +1,119 @@
 require 'Cheetahmails'
 require 'redis'
 
-RSpec.configure do |config|
-  config.before(:each) do
-    begin
-      redis = Redis.new(Cheetahmails.configuration.redis)
-      redis.del "cheetahmails_access_token"
-    rescue => error
-    end
+RSpec.describe Cheetahmails do
+
+  before(:each) do
+    @redis = Redis.new(Cheetahmails.configuration.redis)
+    @redis.del "cheetahmails_access_token"
 
     Cheetahmails.configure do |config|
-      config.username = ENV['USERNAME']
-      config.password = ENV['PASSWORD']
+      config.username = ENV["USERNAME"]
+      config.password = ENV["PASSWORD"]
     end
   end
-end
 
-RSpec.describe Cheetahmails, "#get_token" do
-  context "without being authenticated" do
-    it "authenticating does not return a token when credentials are invalid" do
-      Cheetahmails.configure do |config|
-        config.username = "invalid"
-        config.password = "invalid"
-      end
-      expect { token = Cheetahmails.get_token }.to raise_error("400 Bad Request")
-   end
-    it "authenticating returns a token when credentials are valid" do
-      token = Cheetahmails.get_token
-      expect(token).to be_kind_of(String)
-    end
-    it "authenticating caches a token to redis when credentials are valid" do
-      token = Cheetahmails.get_token
-      expect(token).to be_kind_of(String)
+  context "#get_token" do
+    context "when not authenticated" do
 
-      begin
-        redis = Redis.new(Cheetahmails.configuration.redis)
-        cached_token = redis.get "cheetahmails_access_token"
-
-        expect(cached_token).to eq(token)
-
-        # Expiry on token
-        expect(redis.ttl "cheetahmails_access_token").to be > 28700
-      rescue => error
-        expect(true).to eq(false)
+      it "fails and raises an exception" do
+        Cheetahmails.configure do |config|
+          config.username = "invalid"
+          config.password = "invalid"
+        end
+        expect { token = Cheetahmails.get_token }.to raise_error("400 Bad Request")
       end
 
-    end
-  end
-end
+      context "authenticating" do
+        context "when credentials are valid" do
+          it "authenticating returns a token" do
+            expect(Cheetahmails.get_token).to be_kind_of(String)
+          end
 
-RSpec.describe Cheetahmails, "#find_list_member" do
-  context "with a uniquely generated email address" do
-    it "does not exist in cheetahmail" do
-      exists = Cheetahmails.find_list_member(Time.now.to_f.to_s + "@simpleweb.co.uk")
-      expect(exists).to eq(false)
+          it "caches the token" do
+            token = Cheetahmails.get_token
+            expect(@redis.get("cheetahmails_access_token")).to eq(token)
+            expect(@redis.ttl "cheetahmails_access_token").to be > 28700
+          end
+        end
+      end
     end
   end
-  context "with a known email address" do
-    it "does exist in cheetahmail" do
-      exists = Cheetahmails.find_list_member("tom@simpleweb.co.uk")
-      expect(exists["id"]).to be_kind_of(Integer)
-    end
-  end
-  context "with an invalid access token and a known email address" do
-    # This tests the re-generation of auth token
-    it "does exist in cheetahmail" do
 
-      begin
-        redis = Redis.new(Cheetahmails.configuration.redis)
-        redis.set "cheetahmails_access_token", "invalid token"
-      rescue => error
-        expect(true).to eq(false)
+  context "#find_list_member" do
+    context "with an email address that does not exist in cheetahmail" do
+      it "responds with false" do
+        exists = Cheetahmails.find_list_member(Time.now.to_f.to_s + "@simpleweb.co.uk")
+        expect(exists).to eq(false)
+      end
+    end
+
+    context "with a known email address" do
+      it "responds with the row" do
+        response = Cheetahmails.find_list_member("tom@simpleweb.co.uk")
+        expect(response).to be_kind_of(Hash)
+        expect(response).to include("id", "properties")
+        expect(response["id"]).to be_kind_of(Integer)
       end
 
-      exists = Cheetahmails.find_list_member("tom@simpleweb.co.uk")
-      expect(exists["id"]).to be_kind_of(Integer)
+      context "with an invalid access token" do
+        before do
+          @redis.set "cheetahmails_access_token", "invalid token"
+        end
+
+        it "responds with the row" do
+          response = Cheetahmails.find_list_member("tom@simpleweb.co.uk")
+          expect(response).to be_kind_of(Hash)
+          expect(response).to include("id", "properties")
+          expect(response["id"]).to be_kind_of(Integer)
+        end
+      end
     end
   end
+
+  #   context " token and a known email address" do
+  #     # This tests the re-generation of auth token
+  #     it "does exist in cheetahmail" do
+
+  #       begin
+  #         redis = Redis.new(Cheetahmails.configuration.redis)
+  #
+  #       rescue => error
+  #         expect(true).to eq(false)
+  #       end
+
+  #       exists = Cheetahmails.find_list_member("tom@simpleweb.co.uk")
+  #       expect(exists["id"]).to be_kind_of(Integer)
+  #     end
+  #   end
+  # end
+
+  # context "#add_list_member" do
+  #   context "with a valid view id" do
+  #     it "is possible to add a customer" do
+  #       data = {
+  #         "first_name" => "Tom",
+  #         "last_name" => "Holder",
+  #         "email_address" => "tom@simpleweb.co.uk"
+  #       }
+  #       response = Cheetahmails.add_list_member ENV['VIEW_ID'], data
+  #       expect(response).to eq(true)
+  #     end
+  #   end
+  #   context "without a valid view id" do
+  #     it "is not possible to add a customer" do
+  #       data = {
+  #         "first_name" => "Tom",
+  #         "last_name" => "Holder",
+  #         "email_address" => "tom@simpleweb.co.uk"
+  #       }
+  #       response = Cheetahmails.add_list_member -9999, data
+  #       expect(response).to eq(false)
+  #     end
+  #   end
+  # end
 end
 
-RSpec.describe Cheetahmails, "#add_list_member" do
-  context "with a valid view id" do
-    it "is possible to add a customer" do
-      data = {
-        "first_name" => "Tom",
-        "last_name" => "Holder",
-        "email_address" => "tom@simpleweb.co.uk"
-      }
-      response = Cheetahmails.add_list_member ENV['VIEW_ID'], data
-      expect(response).to eq(true)
-    end
-  end
-  context "without a valid view id" do
-    it "is not possible to add a customer" do
-      data = {
-        "first_name" => "Tom",
-        "last_name" => "Holder",
-        "email_address" => "tom@simpleweb.co.uk"
-      }
-      response = Cheetahmails.add_list_member -9999, data
-      expect(response).to eq(false)
-    end
-  end
-end
+
+
+
